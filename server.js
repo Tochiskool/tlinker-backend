c493e40407df6923a1482c2541392437cfd07067
AChristian N <31229894+Tochiskool@users.noreply.github.com>
2025-06-10 20:51:28 +0200

    took of the trailing slash from the vercel link at the end

--- server.js
+++ server.js
@@ -3,17 +3,18 @@ import data from './data.js';
 import cors from 'cors';
 
 const app = express();
+
 app.use(
   cors({
-    origin: ['http://localhost:5173','http://localhost:5174', 'https://tlinker-frontend.vercel.app/'],
-    credentials: true, 
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'https://tlinker-frontend.vercel.app'],
+    credentials: true,
   })
 );
-
 
 app.get('/api/products', (req, res) => {
   res.send(data.products);
 });
+
 app.get('/api/products/slug/:slug', (req, res) => {
   const product = data.products.find((x) => x.slug === req.params.slug);
   console.log('Sending product:', product);
